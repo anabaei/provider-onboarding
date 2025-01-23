5b67ae38f95753b01d969af409786a9d22da6573
anabaei <nabaei17@gmail.com>
2025-01-22 16:37:56 -0800

    select availability

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import OnboardingForm from './components/onboarding/OnboardingForm';
 import OnboardingStep2 from './components/onboarding2/OnboardingStep2';
 import Services from './components/services/Services';
 import Treatments from './components/treatments/Treatments';
+import WeeklyAvailability from './components/availability/WeeklyAvailability';
 
 const App = () => {
     const [step, setStep] = useState(1);
@@ -24,10 +25,11 @@ const App = () => {
 
     return (
         <div className="App">
-            {step === 3 && <OnboardingForm onNext={nextStep} />}
+            {step === 5 && <OnboardingForm onNext={nextStep} />}
             {step === 2 && <OnboardingStep2 onBack={prevStep} onNext={nextStep} />}
-            {step === 4 && <Services onBack={prevStep} onNext={handleServiceSelect} />}
-            {step === 1 && <Treatments selectedService={selectedService} onBack={prevStep} />}
+            {step === 3 && <Services onBack={prevStep} onNext={handleServiceSelect} />}
+            {step === 4 && <Treatments selectedService={selectedService} onBack={prevStep} onNext={nextStep} />}
+            {step === 1 && <WeeklyAvailability onBack={prevStep} onNext={nextStep} />}
         </div>
     );
 };
